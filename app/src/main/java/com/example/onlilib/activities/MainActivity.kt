533f6e7b67fdd6c2088e3544486f31aec1860026
Bhawna <bhawnabc2002@gmail.com>
2022-06-14 21:46:21 +0530

    complete splash screen work

--- app/src/main/java/com/example/onlilib/activities/MainActivity.kt
+++ app/src/main/java/com/example/onlilib/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.onlilib
+package com.example.onlilib.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.onlilib.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/onlilib/activities/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.example.onlilib.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.example.onlilib.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
+        val backgroundImg: ImageView = findViewById(R.id.iv_logo)
+        backgroundImg.startAnimation(sideAnimation)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2500)
+    }
+}
