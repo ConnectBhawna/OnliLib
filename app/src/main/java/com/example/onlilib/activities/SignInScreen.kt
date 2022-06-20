a8a19a3860d5ab5d43c8fdaa67abeb27f7c767f1
Bhawna <bhawnabc2002@gmail.com>
2022-06-20 22:41:02 +0530

    sign up screen

---
+++ app/src/main/java/com/example/onlilib/activities/SignInScreen.kt
@@ -0,0 +1,12 @@
+package com.example.onlilib.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.onlilib.R
+
+class SignInScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_screen)
+    }
+}
