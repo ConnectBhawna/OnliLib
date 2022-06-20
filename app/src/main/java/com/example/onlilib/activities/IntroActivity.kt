aee4319aebb595bf169a7699c91551f412550de8
Bhawna <bhawnabc2002@gmail.com>
2022-06-20 22:50:16 +0530

    sign up screen

--- app/src/main/java/com/example/onlilib/activities/IntroActivity.kt
+++ app/src/main/java/com/example/onlilib/activities/IntroActivity.kt
@@ -1,12 +1,27 @@
 package com.example.onlilib.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.onlilib.R
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, SignInScreen::class.java))
+        }
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, SignUpScreen::class.java))
+        }
     }
 }
